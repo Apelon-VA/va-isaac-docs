fbbaf42347cfb84cdcdb9876da3a0f43b967ff9a
unknown <vkaloidis@LAPVKALOIDIS.ct.apelon.com>
2015-06-22 13:14:55 -0400

    Added a new script to clone all the projects from GitHub

---
+++ scripts/cloneAll.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+#
+# This script clones each project from GIT
+
+
+import subprocess
+import os
+projects = ['va-ochre',
+			'va-isaac-metadata',
+			'va-isaac-mojo',
+			'va-newtons-cradle',
+			'va-logic',
+			'va-query-service',
+			'va-isaac-gui',
+			'va-solor-goods',
+			'va-expression-service',
+			'va-isaac-gui-pa']
+
+def git(*args):
+	return subprocess.check_call(['git'] + list(args))
+
+#Developers, set this according to your remote name - 'origin' is the git default, but with multiple remotes, the commented out convention below may work better...
+remoteOne = 'origin'
+#remoteOne = 'GH-A'
+#remoteTwo = 'CN'
+
+gitHubURL = 'https://github.com/Apelon-VA/'
+
+for project in projects:
+        if os.path.isdir(os.getcwd() + project):
+                print(os.getcwd() + project + " does not exist")
+                thisProjectUrl = gitHubURL + project + '.git'
+                print("==================================")
+                print("Cloning Latest From " + project)
+                git("clone", thisProjectUrl)
+                print("==================================")
+                print("");
+        else:
+                print(project + " already exists in " + os.getcwd())
