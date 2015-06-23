ec234463450a15031c4ea8887db9f85ae96e3672
darmbrust <daniel.armbrust.list@gmail.com>
2015-06-23 12:05:55 -0500

    fix bugs

--- scripts/cloneAll.py
+++ scripts/cloneAll.py
@@ -5,7 +5,9 @@
 
 import subprocess
 import os
-projects = ['va-ochre',
+projects = ['va-isaac-docs',
+			'va-isaac-parent',
+			'va-ochre',
 			'va-isaac-metadata',
 			'va-isaac-mojo',
 			'va-newtons-cradle',
@@ -17,23 +19,18 @@ projects = ['va-ochre',
 			'va-isaac-gui-pa']
 
 def git(*args):
-	return subprocess.check_call(['git'] + list(args))
-
-#Developers, set this according to your remote name - 'origin' is the git default, but with multiple remotes, the commented out convention below may work better...
-remoteOne = 'origin'
-#remoteOne = 'GH-A'
-#remoteTwo = 'CN'
+		return subprocess.check_call(['git'] + list(args))
 
 gitHubURL = 'https://github.com/Apelon-VA/'
 
 for project in projects:
-        if os.path.isdir(os.getcwd() + project)== False:
-                print(os.getcwd() + project + " does not exist")
-                thisProjectUrl = gitHubURL + project + '.git'
-                print("==================================")
-                print("Cloning Latest From " + project)
-                git("clone", thisProjectUrl)
-                print("==================================")
-                print("");
-        else:
-                print(project + " already exists in " + os.getcwd())
+		if os.path.isdir(os.getcwd() + os.sep + project)== False:
+				print(os.getcwd() + os.sep + project + " does not exist")
+				thisProjectUrl = gitHubURL + project + '.git'
+				print("==================================")
+				print("Cloning Latest From " + project)
+				git("clone", thisProjectUrl)
+				print("==================================")
+				print("");
+		else:
+				print(project + " already exists in " + os.getcwd())
